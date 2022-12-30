8344105e028261dca7ceff01a7b30856ac8146cc
baltazar-christian <baltazarchristian49@gmail.com>
2022-12-31 00:12:16 +0300

    few changes

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -11,7 +11,7 @@
       <!-- Small boxes (Stat box) -->
       <div class="row">
         {{-- Registered Members Reports --}}
-        <div class="col-lg-4 col-6">
+        <div class="col-lg-4 col-sm-6 col-12">
           <!-- small box -->
           <div class="small-box bg-white">
             <div class="inner">
@@ -30,7 +30,7 @@
           </div>
         </div>
         {{-- Collected Payments Reports --}}
-        <div class="col-lg-4 col-6">
+        <div class="col-lg-4 col-sm-6 col-12">
             <!-- small box -->
             <div class="small-box bg-white">
               <div class="inner">
@@ -48,7 +48,7 @@
             </div>
           </div>
         {{-- Collected Payments Reports --}}
-        <div class="col-lg-4 col-6">
+        <div class="col-lg-4 col-12">
             <!-- small box -->
             <div class="small-box bg-white">
               <div class="inner">
@@ -61,10 +61,14 @@
                 <i class="fa fa-balance-scale text-danger"></i>
               </div>
               <br>
-              <a href="" class="mt-4 small-box-footer" type="button"  data-bs-toggle="modal" data-bs-target="#paymentModal">Generate Report <i class="fas fa-download text-primary"></i></a>
+              <a href="" class="mt-4 small-box-footer" type="button"  data-bs-toggle="modal" data-bs-target="#pledgesModal">Generate Report <i class="fas fa-download text-primary"></i></a>
             </div>
           </div>
 
+
+          <div class="col-m-12">
+            <hr class="bg-primary font-weight-bolder">
+          </div>
       </div>
     </div>
 </section>
@@ -176,7 +180,7 @@
   
 
 {{-- Pledges/Purpose modal --}}
-<div class="modal fade" id="paymentModal" tabindex="-1" >
+<div class="modal fade" id="pledgesModal" tabindex="-1" >
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header bg-light">
@@ -184,13 +188,25 @@
           <button type="button" class="btn-close btn-danger btn-sm" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form action="{{ url('admin/collected-payments') }}" method="GET">
+          <form action="{{ url('admin/pledges-purposes') }}" method="GET">
             @csrf
     
             <div class="mb-3">
             <h5 class="text-secondary">
-                Generate Collected Payments Report From the Given Start Date To the given End Date
+                Generate Pledges Per Purpose Report From the Given Start Date To the given End Date
             </h5>
+            </div>
+            @php
+            $purpose= App\Models\Purpose::get();
+            @endphp
+            <div class="mb-3">
+                <label for="" class="text-secondary">Choose Purpose</label>
+                <select name="pledge_id" id="pledge_id" class="form-control">
+                    <option value="">--Select Purpose --</option>
+                    @foreach ( $purpose as $item)
+                    <option value="{{ $item->id}}">{{ $item->title}} </option>
+                    @endforeach
+                </select>
             </div>
             <div class="mb-3">
               <label for="message-text" class="text-secondary">From Date:</label>
@@ -203,9 +219,10 @@
             <div class="mb-3">
                 <label for="message-text" class="text-secondary">Sort By:</label>
                 <select name="sort_by" id="sort_by">
-                    <option value="created_at">Collected Date</option>
-                    <option value="user_id">Payer</option>
+                    <option value="created_at">Created Date</option>
+                    <option value="user_id">Member</option>
                     <option value="pledge_id">Purpose</option>
+                    <option value="type_id">Pledge Type</option>
                 </select>
               </div>
             <div class="row">
