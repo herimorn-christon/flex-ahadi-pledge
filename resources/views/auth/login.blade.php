d8daaf07872f4037ee570e0f349d722fbb006e5b
baltazar-christian <baltazarchristian49@gmail.com>
2023-01-08 14:08:09 +0300

    cleaned login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,4 @@
+<!--This is the view for user login page -->
 @extends('layouts.app')
 
 @section('content')
@@ -9,14 +10,17 @@
             <div class="card-body">
             
                 <div class="py-4 mt-2">
-                    <div class="col-md-5 col-sm-5 col-5 mx-auto">
+                  {{-- start of flex logo --}} 
+                   <div class="col-md-5 col-sm-5 col-5 mx-auto">
                         <img src="{{ asset('img/logoshorts.png') }}" alt="Flex Logo" class="" width="100%" height="60px">
                     </div>
+                  {{-- start of flex logo --}} 
+
                     <h4 class="text-center text-flex py-2">
                         <span class="font-weight-bolder">AhadiPledge</span> 
                     </h4>
                 </div>
-
+                  {{-- start of login form  --}}
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
     
@@ -25,7 +29,7 @@
                        
                         <div class="input-group mb-3">
                            
-                            <input type="text" class="form-control " name="phone" id="phone" placeholder="Phone Number/Nambari ya Simu" aria-label="Username" aria-describedby="basic-addon1">
+                            <input type="text" class="form-control " name="phone" id="phone" placeholder="Phone Number/Nambari ya Simu" aria-label="Username" aria-describedby="basic-addon1" required>
                             <span class="input-group-text" id="basic-addon1"><i class="fa fa-phone"></i></span>
                         
                         </div>
@@ -37,13 +41,14 @@
     
                         <div class="input-group mb-32">
                             
-                            <input type="password" class="form-control" name="password" id="password" placeholder="Password/Neno siri" aria-label="Password" aria-describedby="basic-addon1" required autocomplete="current-password">
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password/Neno siri" aria-label="Password" aria-describedby="basic-addon1" required  required>
                             <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
                       
                         </div>
                     </div>
     
                     <div class="row mb-3">
+                      {{-- start displaying errors --}}
                       @if ($errors->any())
                       <div class="btn btn-danger disabled btn-block mb-3">
                           @foreach ($errors->all() as $error)
@@ -51,6 +56,7 @@
                           @endforeach
                       </div>
                       @endif
+                       {{--end  displaying errors --}}
                         <div class="col-md-5 offset-md-1">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -70,6 +76,7 @@
     
                     </div>
                 </form>
+                  {{-- end of login form  --}}
             </div>
           
         </div>
