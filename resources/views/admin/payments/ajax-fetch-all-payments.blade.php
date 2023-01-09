f0bb9109f12caa81d46dda51ffd7e806865a0bbe
baltazar-christian <baltazarchristian49@gmail.com>
2023-01-09 15:08:33 +0300

    admin-manage-payments-changes

--- resources/views/admin/payments/ajax-fetch-all-payments.blade.php
+++ resources/views/admin/payments/ajax-fetch-all-payments.blade.php
@@ -1,50 +1,53 @@
 {{-- This is the page for ajax to fetch all registered Purposes/Contributions --}}
 
 <script type="text/javascript">
-    showAllPurposes();
-         
-            /*
-                This function will get all the purposes records
-            */
-            function showAllPurposes()
-            {
-                let url = $('meta[name=app-url]').attr("content") + "/admin/purposes";
-                $.ajax({
-                    url: url,
-                    type: "GET",
-                    success: function(response) {
-                        $("#purposes-table-body").html("");
-                        let purposes = response.purposes;
-                        for (var i = 0; i < purposes.length; i++) 
-                        {
-                            let showBtn =  '<button ' +
-                                ' class="btn btn-sm bg-flex text-light" ' +
-                                ' onclick="showPurpose(' + purposes[i].id + ')" data-toggle="tooltip" data-placement="bottom" title="Click here to View '+ purposes[i].title +' details."><i class="fa fa-eye"></i>' +
-                            '</button> ';
-                            let editBtn =  '<button ' +
-                                ' class="btn btn-sm bg-flex text-light" ' +
-                                ' onclick="editPurpose(' + purposes[i].id + ')" data-toggle="tooltip" data-placement="bottom" title="Click here to Update '+ purposes[i].title +' details."><i class="fa fa-edit"></i>' +
-                            '</button> ';
-                            let deleteBtn =  '<button ' +
-                                ' class="btn btn-sm btn-danger" ' +
-                                ' onclick="destroyPurpose(' + purposes[i].id + ')" data-toggle="tooltip" data-placement="bottom" title="Click here to Delete '+ purposes[i].title +'."><i class="fa fa-trash"></i>' +
-                            '</button>';
-         
-                            let projectRow = '<tr>' +
-                                '<td>' + (i+1) + '</td>' +
-                                '<td>' + purposes[i].title + '</td>' +
-                                '<td>' + purposes[i].start_date + '</td>' +
-                                '<td>' + purposes[i].end_date + '</td>' +
-                                '<td>' + showBtn + editBtn + deleteBtn + '</td>' +
-                            '</tr>';
-                            $("#purposes-table-body").append(projectRow);
-                        }
-         
-                         
-                    },
-                    error: function(response) {
-                        console.log(response.responseJSON)
-                    }
-                });
-            }        
+    
+
+
+        showAllPledges();
+    
+        /*
+            This function will get all the payments records
+        */
+        function showAllPledges()
+        {
+            let url = $('meta[name=app-url]').attr("content") + "/admin/payments";
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    $("#projects-table-body").html("");
+                    let purposes = response.purposes;
+                    for (var i = 0; i < purposes.length; i++) 
+                    {
+                        let showBtn =  '<button ' +
+                            ' class="btn btn-sm bg-navy" ' +
+                            ' onclick="showPledge(' + purposes[i].id + ')"><i class="fa fa-eye"></i>' +
+                        '</button> ';
+                        let editBtn =  '<button ' +
+                            ' class="btn btn-sm bg-navy" ' +
+                            ' onclick="editPledge(' + purposes[i].id + ')"><i class="fa fa-edit"></i>' +
+                        '</button> ';
+                        let deleteBtn =  '<button ' +
+                            ' class="btn btn-danger btn-sm" ' +
+                            ' onclick="destroyPledge(' + purposes[i].id + ')"><i class="fa fa-trash"></i>' +
+                        '</button>';
+     
+                        let projectRow = '<tr>' +
+                            '<td>' + purposes[i].payer.fname + '&nbsp;' + purposes[i].payer.mname +  '&nbsp;' + purposes[i].payer.lname +   '</td>' +
+                            '<td>' + purposes[i].payment.name + '</td>' +
+                            '<td>' + purposes[i].pledge.name + '</td>' +
+                            '<td>' + purposes[i].amount + '</td>' +
+                            '<td>' + showBtn + editBtn + deleteBtn + '</td>' +
+                        '</tr>';
+                        $("#projects-table-body").append(projectRow);
+                    }
+     
+                     
+                },
+                error: function(response) {
+                    console.log(response.responseJSON)
+                }
+            });
+        }
 </script>
--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -301,54 +301,6 @@
   {{-- end of auto populate member pledges --}}
    <script type="text/javascript">
   
-
-
-            showAllPledges();
-        
-            /*
-                This function will get all the payments records
-            */
-            function showAllPledges()
-            {
-                let url = $('meta[name=app-url]').attr("content") + "/admin/payments";
-                $.ajax({
-                    url: url,
-                    type: "GET",
-                    success: function(response) {
-                        $("#projects-table-body").html("");
-                        let purposes = response.purposes;
-                        for (var i = 0; i < purposes.length; i++) 
-                        {
-                            let showBtn =  '<button ' +
-                                ' class="btn btn-sm bg-navy" ' +
-                                ' onclick="showPledge(' + purposes[i].id + ')"><i class="fa fa-eye"></i>' +
-                            '</button> ';
-                            let editBtn =  '<button ' +
-                                ' class="btn btn-sm bg-navy" ' +
-                                ' onclick="editPledge(' + purposes[i].id + ')"><i class="fa fa-edit"></i>' +
-                            '</button> ';
-                            let deleteBtn =  '<button ' +
-                                ' class="btn btn-danger btn-sm" ' +
-                                ' onclick="destroyPledge(' + purposes[i].id + ')"><i class="fa fa-trash"></i>' +
-                            '</button>';
-         
-                            let projectRow = '<tr>' +
-                                '<td>' + purposes[i].payer.fname + '&nbsp;' + purposes[i].payer.mname +  '&nbsp;' + purposes[i].payer.lname +   '</td>' +
-                                '<td>' + purposes[i].payment.name + '</td>' +
-                                '<td>' + purposes[i].pledge.name + '</td>' +
-                                '<td>' + purposes[i].amount + '</td>' +
-                                '<td>' + showBtn + editBtn + deleteBtn + '</td>' +
-                            '</tr>';
-                            $("#projects-table-body").append(projectRow);
-                        }
-         
-                         
-                    },
-                    error: function(response) {
-                        console.log(response.responseJSON)
-                    }
-                });
-            }
          
 
     //         showAllMethods();
