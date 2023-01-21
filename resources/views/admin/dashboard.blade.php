e24a0f874fd05feaae89a8e2718146f1b81bd289
baltazar-christian <baltazarchristian49@gmail.com>
2023-01-21 18:30:07 +0300

    changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,6 +4,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AhadiPledge| Dashboard</title>
+
+  @php
+  $setting= App\Models\Setting::get()->first();
+  @endphp
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
   <!-- Google Font: Source Sans Pro -->
